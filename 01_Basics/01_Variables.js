59e7fd571f0bfbe1063cf0ef1da95a3b98d110a6
Tisha-Chauhan123 <23ce356.tisha.chauhan@vvpedulink.ac.in>
2025-04-15 06:54:18 +0000

    container config done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 422335
+let accountEmail = "tctc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 //
+
+// console.log(accountId) //
+
+accountEmail = "tisha@gmail.com"
+accountPassword = "76576"
+accountCity = "Rajkot"
+
+console.table(accountId,accountEmail,accountPassword,accountState)
