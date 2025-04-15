6c2518e72aba6ace54e02e3d1c301bb542193b90
Tisha-Chauhan123 <23ce356.tisha.chauhan@vvpedulink.ac.in>
2025-04-15 18:58:31 +0530

    Update 03_conversionDatatypes.js

--- 01_Basics/03_conversionDatatypes.js
+++ 01_Basics/03_conversionDatatypes.js
@@ -17,3 +17,21 @@ console.log(valueChanged)
 let isLoggedIn = 1
 let BooleanisLoggedIn = Boolean(isLoggedIn)
 console.log(typeof (BooleanisLoggedIn))
+
+let str = "Tisha"
+let str1 = "Chauhan"
+let str3 = str1 + str2
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+
+console.log((3 + 4) * 5 / 3);
