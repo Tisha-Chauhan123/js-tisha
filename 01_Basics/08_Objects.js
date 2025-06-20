de4817bba1bff39f512834516eaef401fb3059a0
Tisha-Chauhan123 <23ce356.tisha.chauhan@vvpedulink.ac.in>
2025-06-20 12:23:28 +0530

    data_saved

---
+++ 01_Basics/08_Objects.js
@@ -0,0 +1,18 @@
+const tinderUser = {}
+
+tinderUser.id = "12345"
+tinderUser.name = "Tishaa"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser)
+
+const anotherUser = {
+    fullname:{
+        userfullname:{
+            firstName: "Tishaa"
+            lastName: "Chauhan"
+        }
+    }
+}
+
+console.log(anotherUser.fullname);
